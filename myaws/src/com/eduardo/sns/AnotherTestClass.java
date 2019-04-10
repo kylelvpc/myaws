70d3e9d29ac86b75f07f04f7f9a570be1552ffaf
kylelvpc <1537811563@qq.com>
2019-04-10 21:20:50 +0800

    ...

---
+++ myaws/src/com/eduardo/sns/AnotherTestClass.java
@@ -0,0 +1,14 @@
+package com.eduardo.sns;
+
+public class AnotherTestClass {
+
+	public AnotherTestClass() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
